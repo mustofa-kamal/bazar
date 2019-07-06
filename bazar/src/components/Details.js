39378d5cbe3bd813aeb72abcdd581daa84f2bea4
mustofakamal <mkamal@igniteinsurance.ca>
2019-07-06 13:08:12 +0600

    saving

---
+++ bazar/src/components/Details.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+
+
+export default function Details() {
+  return (
+    <Typography variant="subtitle1" color="primary">
+            Details page
+    </Typography>
+  
+  
+  );
+
+}
